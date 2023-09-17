8ceb68c2b41d208739b1302c0d276bd9e0cb5f84
Mr-Donot <florian.daunay37@gmail.com>
2023-09-17 16:03:01 +0200

    Update scrapper

--- Scrapper.py
+++ Scrapper.py
@@ -4,11 +4,17 @@ from bs4 import BeautifulSoup
 
 class Scrapper:
 
-    def __init__(self, url):
+    def __init__(self, url=None):
         self.url = url
 
-    def getPageTxt(self):
-        page = requests.get(self.url)
+    def getPageTxt(self, url=None):
+        if url is None :
+            if self.url is None :
+                print("Need an URL to scrap.")
+                return False
+            page = requests.get(self.url)
+        else:
+            page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         result = soup.find(id="mw-content-text")
         r = result.find(class_="mw-parser-output")
@@ -21,9 +27,9 @@ class Scrapper:
 
         return "".join(tabResult)
     
-    def saveTextInFile(self):
-        text = self.getPageTxt()
-        
+    def saveTextInFile(self, url=None):
+        text = self.getPageTxt(url)
+        if not text : return
         #TODO recup le titre de la page pour le nom du fichier
         fileName = "data/abc.txt"
         
